0876a39b8ff2d8d700157b5715e79b1bad9394b1
Christopher Vestergaard Jensen <cvje61684@edu.ucl.dk>
2024-04-04 11:32:53 +0200

    HAHAHAHAHAHA

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -1,11 +1,9 @@
-﻿//SUP FUCKERS!
-int[] monthlyPay = { 10000, 20000, 15000, 20000, 30000, 15000 };
-double sum = 0;
-for (int i = 0; i < monthlyPay.Length; i++)
+﻿double Sum(int[] array)
 {
-    sum += monthlyPay[i];
-    Console.WriteLine(monthlyPay[i]);
+    double sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        sum += array[i];
+    }
+    return sum;
 }
-
-double average = sum / monthlyPay.Length;
-Console.WriteLine("Gennemsnit: " + average.ToString("N2"));
