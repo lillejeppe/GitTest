21103a3fbc54e70c7d6b3c6207d970b758d9d5ba
Jeppe Lund <jelu61681@edu.ucl.dk>
2024-04-04 10:29:44 +0200

    HELP!!!

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -1,2 +1,13 @@
 ﻿//SUP FUCKERS!
 //BRANCH TEST
+
+Console.WriteLine("Jeg spiser fisk for sjov :)");
+
+int number = 0;
+for  (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(number);
+    number++;
+}
+
+Console.WriteLine("Fladfisk kan flyve");
